5780a1ecec77ccfacf05f1732c90363478c34fcf
Peter Holmberg <peter.hlmbrg@gmail.com>
2018-10-16 22:11:22 +0200

    updated bikes

--- src/app/containers/bikes/bikes.jsx
+++ src/app/containers/bikes/bikes.jsx
@@ -1,63 +1,71 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import BikeGrid from 'components/bikes/bike-grid/bike-grid';
-import NoBikes from 'components/bikes/no-bikes/no-bikes';
-import Loader from 'components/viewHelper/loader';
-import { loadBikes, clearBike } from 'actions/bike-actions';
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import BikeGrid from 'components/bikes/bike-grid/bike-grid'
+import NoBikes from 'components/bikes/no-bikes/no-bikes'
+import Loader from 'components/viewHelper/loader'
+import { loadBikes, clearBike } from 'actions/bike-actions'
 
-class Bikes extends Component {
-    componentDidMount() {
-        const { bikes, user } = this.props;
+class Bikes extends PureComponent {
+  componentDidMount () {
+    const {user, loadBikes} = this.props
 
-        if (!user.isEmpty() && bikes.isEmpty()) {
-            this.props.loadBikes(user.get('instagram'));
-        }
+    if (!user.isEmpty()) {
+      loadBikes(user.get('instagram'))
+    }
+  }
+
+  componentWillUnmount () {
+    this.props.clearBike()
+  }
+
+  static getDerivedStateFromProps (nextProps) {
+    if (!nextProps.user.isEmpty() && nextProps.bikes.isEmpty()) {
+      nextProps.loadBikes(nextProps.user.get('instagram'))
+    }
+  }
+
+  render () {
+    const {authenticated, bikes, networkProgress} = this.props
+    if (!authenticated) {
+      return (
+        <Redirect to={{pathname: '/login'}} />
+      )
     }
 
-    componentWillUnmount() {
-        this.props.clearBike();
+    if (networkProgress) {
+      return (
+        <Loader />
+      )
     }
 
-    render() {
-        const { authenticated, bikes, networkProgress } = this.props;
-        if (!authenticated) {
-            return (
-                <Redirect to={{ pathname: '/login' }} />
-            );
-        }
-
-        if (networkProgress) {
-            return (
-                <Loader />
-            );
-        }
-
-        if (bikes.isEmpty()) {
-            return (
-                <NoBikes />
-            );
-        }
-        else {
-            return (
-                <BikeGrid bikes={bikes} />
-            );
-        }
+    if (bikes.isEmpty()) {
+      return (
+        <NoBikes />
+      )
     }
+    else {
+      return (
+        <BikeGrid bikes={bikes} />
+      )
+    }
+  }
 }
 
-function propProvider(reduxState) {
-    const { appState, userState, bikeState } = reduxState;
+function mapStateToProps (reduxState) {
+  const {appState, userState, bikeState} = reduxState
 
-    return {
-        authenticated: appState.get('authenticated'),
-        bikes: bikeState.get('bikes'),
-        user: userState.get('user'),
-        networkProgress: appState.get('networkProgress'),
-    };
+  return {
+    authenticated: appState.get('authenticated'),
+    bikes: bikeState.get('bikes'),
+    user: userState.get('user'),
+    networkProgress: appState.get('networkProgress'),
+  }
 }
 
-export default connect(propProvider, {
-    loadBikes,
-    clearBike,
-})(Bikes);
+const mapDispatchToProps = {
+  loadBikes,
+  clearBike,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bikes)
